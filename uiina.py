54572cdbd914deddd2304b4cd1dc8d82407dbb23
ed9w2in6 <win1998926@gmail.com>
2023-10-14 16:06:33 +0800

    feat: add stdin handler + rewritten with modern python syntax
    Allows stdin if a single file `-` is submitted.

--- uiina.py
+++ uiina.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 """
+Edit (welkinSL):  [2023-10-14] - rewrite
 Edit (welkinSL):  I simply replaced all mentions of 'mpv' with 'IINA' in the instruction 
                   below, and removed irrelavent sections since the mechanism and behaviour
                   should be almost identical.  The only differences should be that IINA 
@@ -31,22 +32,27 @@ Note: you can supply custom IINA path and options with the IINA environment
       is already running), this will be ignored.
 """
 
-import sys
-import os
-import getopt
-import socket
-import subprocess
-import string
-import textwrap
-import errno
-import signal
 import atexit
+import errno
+import getopt
+import os
+import signal
+import socket
+import string
+import subprocess
+import sys
+import textwrap
+from pathlib import Path
+
 
 def print_quick_help():
-    print('Usage: uiina [-vh] [file ...]')
+    print("Usage: uiina [-vh] [file ...]")
+
 
 def print_help():
-    print( textwrap.dedent("""\
+    print(
+        textwrap.dedent(
+            """\
         Usage: uiina [option] [file ...]
 
             option:
@@ -56,27 +62,10 @@ def print_help():
             file ...:
                The file(s) to be opened by uiina.
 
-            Inspect the python script for a more detailed explaination."""))
-    
-is_quiet = True
-try:
-    opts, args = getopt.getopt(sys.argv[1:], "hv", ["help", "verbose"])
-except getopt.GetoptError:
-    print('ERROR: Invalid options.')
-    print_quick_help()
-    sys.exit(2)
+            Inspect the python script for a more detailed explaination."""
+        )
+    )
 
-for opt, arg in opts:
-    if opt in ['-h', '--help']:
-        print_help()
-        sys.exit(0)
-    elif opt in ['-v', '--verbose']:
-        is_quiet = False
-    else:
-        assert False, "Invalid Options."
-        print_quick_help()
-    
-files = args 
 
 # this is the same method mpv uses to decide this
 def is_url(filename):
@@ -84,65 +73,102 @@ def is_url(filename):
     if len(parts) < 2:
         return False
     # protocol prefix has no special characters => it's an URL
-    allowed_symbols = string.ascii_letters + string.digits + '_'
+    allowed_symbols = string.ascii_letters + string.digits + "_"
     prefix = parts[0]
     return all(map(lambda c: c in allowed_symbols, prefix))
 
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "hv", ["help", "verbose"])
+except getopt.GetoptError:
+    print("ERROR: Invalid options.")
+    print_quick_help()
+    sys.exit(2)
+
+IS_QUIET = True
+for opt, arg in opts:
+    if opt in ["-h", "--help"]:
+        print_help()
+        sys.exit(0)
+    elif opt in ["-v", "--verbose"]:
+        IS_QUIET = False
+    else:
+        assert False, "Invalid Options."
+        print_quick_help()
+
 # make them absolute; also makes them safe against interpretation as options
-def make_abs(filename):
-    if not is_url(filename):
-        return os.path.abspath(filename)
-    return filename
-files = (make_abs(f) for f in files)
+files = (
+    []
+    if (len(args) == 0) and (args[0] == "-")
+    else (
+        filename if is_url(filename) else Path(filename).expanduser().absolute()
+        for filename in args
+    )
+)
 
-def sigint_handler(signo, frame):
-    is_quiet or print(f'\nReceived signal number { signo }, from frame { frame }.')
-    is_quiet or print('Exiting normally.')
-    sys.exit(0)
-
-SOCK = os.path.join(os.getenv("HOME"), ".uiina_socket")
-
-def remove_uiina_socket_artefacts():
-    is_quiet or print(f'Exiting uiina.\nRemoving file at "{SOCK}".')
-    os.unlink( SOCK )
-    is_quiet or print('Socket file removed.\nExit now.')
-    
+SOCK_PATH = Path.home() / ".uiina_socket"
 sock = None
 try:
     sock = socket.socket(socket.AF_UNIX)
-    sock.connect(SOCK)
-    is_quiet or print(f'Using socket: {sock}')
+    sock.connect(SOCK_PATH.as_posix())
+    if not IS_QUIET:
+        print(f"Using socket: {sock}")
 except socket.error as e:
     if e.errno == errno.ECONNREFUSED:
         sock = None
         pass  # abandoned socket
     elif e.errno == errno.ENOENT:
         sock = None
-        pass # doesn't exist
+        pass  # doesn't exist
     else:
         raise e
 
-if sock:
-    # Unhandled race condition: what if mpv is terminating right now?
-    for f in files:
-        # escape: \ \n "
-        f = f.replace("\\", "\\\\").replace("\"", "\\\"").replace("\n", "\\n")
-        f = "\"" + f + "\""
-        sock.send(("raw loadfile " + f + " append\n").encode("utf-8"))
-else:
+
+def sigint_handler(signo, frame):
+    if not IS_QUIET:
+        print(f"\nReceived signal number { signo }, from frame { frame }.")
+        print("Exiting normally.")
+    sys.exit(0)
+
+
+def remove_uiina_socket_artefacts():
+    if not IS_QUIET:
+        print(f'Exiting uiina.\nRemoving file at "{SOCK_PATH}".')
+    SOCK_PATH.unlink()
+    if not IS_QUIET:
+        print("Socket file removed.\nExit now.")
+
+
+if sock is None:
     # Let mpv recreate socket if it doesn't already exist.
 
-    # Add handlers to clean artefacts ( the file at `SOCK` ) on exit and interrupted.
+    # Add handlers to clean artefacts ( the file at `SOCK_PATH` ) on exit and interrupted.
     # Note that adding them here means they are only added if we need to launch an IINA instance.
-    atexit.register( remove_uiina_socket_artefacts )
+    atexit.register(remove_uiina_socket_artefacts)
     signal.signal(signal.SIGINT, sigint_handler)
-    
+
     opts = (os.getenv("IINA") or "iina").split()
-    
+
+    stdin_opt = "--stdin" if len(list(files)) == 0 else "--no-stdin"
     # append replaced `--` with `--mpv-` as per instructions of iina-cli.  See: `iina --help`.
-    opts.extend(["--mpv-no-terminal", "--mpv-force-window", "--mpv-input-ipc-server=" + SOCK,
-                 "--no-stdin", "--keep-running", # Need this to allow artefacts cleaning. See `iina --help`.
-                 "--"]) # <- Didn't change this one.
+    opts.extend(
+        [
+            "--mpv-no-terminal",
+            "--mpv-force-window",
+            "--mpv-input-ipc-server=" + SOCK_PATH.as_posix(),
+            stdin_opt,
+            "--keep-running",  # Need this to allow artefacts cleaning. See `iina --help`.
+            "--",
+        ]
+    )  # <- Didn't change this one.
     opts.extend(files)
 
     subprocess.check_call(opts)
+else:
+    # Unhandled race condition: what if mpv is terminating right now?
+    for f in files:
+        # escape: \ \n "
+        fname = (
+            f.as_posix().replace("\\", r"\\").replace('"', r"\"").replace("\n", r"\n")
+        )
+        sock.send((f'raw loadfile "{fname}" append\n').encode("utf-8"))
