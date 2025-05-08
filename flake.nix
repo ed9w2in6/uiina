5973ccc9c585f0dc8804c755ae6d9a4d7738dab2
ed9w2in6 <22890124+ed9w2in6@users.noreply.github.com>
2025-05-08 15:05:21 +0800

    build: rebase flake-parts to master

--- flake.nix
+++ flake.nix
@@ -2,202 +2,63 @@
   description = "uiina :: Using single-instance of IINA when launching through the command line";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
-    pyproject-nix = {
-      url = "github:pyproject-nix/pyproject.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    uv2nix = {
-      url = "github:pyproject-nix/uv2nix";
-      inputs.pyproject-nix.follows = "pyproject-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    pyproject-build-systems = {
-      url = "github:pyproject-nix/build-system-pkgs";
-      inputs.pyproject-nix.follows = "pyproject-nix";
-      inputs.uv2nix.follows = "uv2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    # TODO: do we need to name it as nixpkgs here?
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      uv2nix,
-      pyproject-nix,
-      pyproject-build-systems,
-      ...
-    }:
-    let
-      inherit (nixpkgs) lib;
-      inherit (pkgs.callPackages pyproject-nix.build.util { }) mkApplication;
+    { flake-parts, nixpkgs-unstable, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+      ];
+      systems = nixpkgs-unstable.lib.platforms.darwin;
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          pythonPackages = pkgs.python313Packages;
+        in
+        {
+          packages.uiina = import ./package.nix {};
+          packages.default = config.packages.uiina;
+          defaultPackage = config.packages.uiina;
 
-      # Load a uv workspace from a workspace root.
-      # Uv2nix treats all uv projects as workspace projects.
-      workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+          devShells.default = pkgs.mkShell {
+            name = "uiina-dev-env";
+            hardeningDisable = ["all"]; # https://discourse.nixos.org/t/why-is-the-nix-compiled-python-slower
+            packages = with pkgs; [
+              gcc14
+              stdenv.cc.cc.lib
+              pythonPackages.python
+              # pythonPackages.venvShellHook
+              pythonPackages.uv
 
-      # Create package overlay from workspace.
-      overlay = workspace.mkPyprojectOverlay {
-        # Prefer prebuilt binary wheels as a package source.
-        # Sdists are less likely to "just work" because of the metadata missing from uv.lock.
-        # Binary wheels are more likely to, but may still require overrides for library dependencies.
-        sourcePreference = "wheel"; # OR "sdist";
-        # Optionally customise PEP 508 environment
-        # environ = {
-        #   platform_release = "5.10.65";
-        # };
-      };
-
-      # Extend generated overlay with build fixups
-      #
-      # Uv2nix can only work with what it has, and uv.lock is missing essential metadata to perform some builds.
-      # This is an additional overlay implementing build fixups.
-      # See:
-      # - https://pyproject-nix.github.io/uv2nix/FAQ.html
-      pyprojectOverrides = _final: _prev: {
-        # Implement build fixups here.
-        # Note that uv2nix is _not_ using Nixpkgs buildPythonPackage.
-        # It's using https://pyproject-nix.github.io/pyproject.nix/build.html
-      };
-
-      # This example is only using x86_64-darwin
-      pkgs = nixpkgs.legacyPackages.x86_64-darwin;
-
-      python = pkgs.python313;
-
-      # Construct package set
-      pythonSet =
-        # Use base package set from pyproject.nix builders
-        (pkgs.callPackage pyproject-nix.build.packages {
-          inherit python;
-        }).overrideScope
-          (
-            lib.composeManyExtensions [
-              pyproject-build-systems.overlays.default
-              overlay
-              pyprojectOverrides
-            ]
-          );
-
-    in
-    {
-      # Package a virtual environment as our main application.
-      #
-      # mkApplication only links package content present in pythonSet.uiina
-      #
-      # This means that files such as:
-      # - Python interpreters
-      # - Activation scripts
-      # - pyvenv.cfg
-      #
-      # Are excluded but things like binaries, man pages, systemd units etc are included.
-      packages.x86_64-darwin.default = mkApplication {
-        venv = pythonSet.mkVirtualEnv "uiina-min" workspace.deps.default; # workspace.deps.default :: use no optional dependencies
-        package = pythonSet.uiina;
-      };
-      packages.x86_64-darwin.wheel = pythonSet.uiina.override {
-        pyprojectHook = pythonSet.pyprojectDistHook;
-      };
-      packages.x86_64-darwin.shiv = pkgs.callPackage ./package.nix { inherit python; }; # TODO: maybe use mkVirtualEnv's python? is it better?
-
-      # Make uiina runnable with `nix run`
-      apps.x86_64-darwin = {
-        default = {
-          type = "app";
-          program = "${self.packages.x86_64-darwin.default}/bin/uiina";
-          description = "Run uiina, CLI tool that helps using a single-instance of IINA, based on umpv.";
-        };
-      };
-
-      # - Pure development using uv2nix to manage virtual environments
-      devShells.x86_64-darwin = {
-        # we apply another overlay here enabling editable mode: https://setuptools.pypa.io/en/latest/userguide/development_mode.html
-        # This means changes to your local files do NOT trigger a rebuild.
-        #
-        # WARNING: this feature is unstable
-        default =
-          let
-            # Create an overlay enabling editable mode for all local dependencies.
-            editableOverlay = workspace.mkEditablePyprojectOverlay {
-              # Use environment variable
-              root = "$REPO_ROOT";
-              # Optional: Only enable editable for these packages
-              # members = [ "uiina" ];
-            };
-
-            # Override previous set with our overrideable overlay.
-            editablePythonSet = pythonSet.overrideScope (
-              lib.composeManyExtensions [
-                editableOverlay
-
-                # Apply fixups for building an editable package of your workspace packages
-                (final: prev: {
-                  uiina = prev.uiina.overrideAttrs (old: {
-                    # It's a good idea to filter the sources going into an editable build
-                    # so the editable package doesn't have to be rebuilt on every change.
-                    src = lib.fileset.toSource {
-                      root = old.src;
-                      fileset = lib.fileset.unions [
-                        (old.src + "/pyproject.toml")
-                        (old.src + "/README.org")
-                        (old.src + "/COPYING")
-                        (old.src + "/uiina/__init__.py")
-                        # (old.src + "/uiina/uiina.py")
-                      ];
-                    };
-
-                    # Hatchling (our build system) has a dependency on the `editables` package when building editables.
-                    #
-                    # In normal Python flows this dependency is dynamically handled, and doesn't need to be explicitly declared.
-                    # This behaviour is documented in PEP-660.
-                    #
-                    # With Nix the dependency needs to be explicitly declared.
-                    nativeBuildInputs =
-                      old.nativeBuildInputs
-                      ++ final.resolveBuildSystem {
-                        editables = [ ];
-                      };
-                  });
-
-                })
-              ]
-            );
-
-            # Build virtual environment, with local packages being editable.
-            #
-            # Enable all optional dependencies for development.
-            virtualenv = editablePythonSet.mkVirtualEnv "uiina-dev" {
-              uiina = [ "dev" ];
-            };
-          in
-          pkgs.mkShell {
-            name = "uiina-dev"; # TODO: we abuse nix behaviour which append -env to name, may bad idea if its private
-            packages = [
-              virtualenv
-              pkgs.uv
+              # non python deps 
+              # ... e.g. glib, libz, zlib for numpy ...
+              
+              iina
+              config.packages.uiina
             ];
-
-            env = {
-              # Don't create venv using uv
-              UV_NO_SYNC = "1";
-              # Force uv to use Python interpreter from venv
-              UV_PYTHON = "${virtualenv}/bin/python";
-              # Prevent uv from downloading managed Python's
-              UV_PYTHON_DOWNLOADS = "never";
-            };
-
+            # venvDir = "./.venv";
             shellHook = ''
-              # Undo dependency propagation by nixpkgs.
-              unset PYTHONPATH
-
-              # Get repository root using git. This is expanded at runtime by the editable `.pth` machinery.
-              export REPO_ROOT=$(git rev-parse --show-toplevel)
+              export PYTHONPATH="$(pwd)"
             '';
+            LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib";
+            CMAKE_CXX_COMPILER="${pkgs.gcc14}/bin/:${pkgs.clang_18}/bin/";
           };
+
+          # checks.build = ;
+
+          formatter = pkgs.nixfmt-rfc-style;
+        };
+      flake = {
       };
     };
 }
