f2557ab76929180c19fdd1b5d13b8d476cec4f36
ed9w2in6 <22890124+ed9w2in6@users.noreply.github.com>
2025-05-09 02:31:27 +0800

    ci: fix error message for do_or_fatal + early exit on failure

--- ci_scripts/prepare-release.sh
+++ ci_scripts/prepare-release.sh
@@ -4,20 +4,24 @@ do_or_fatal() {
     __do_or_fatal__cmd="$1"
     shift # "$@" is REST after this
     "$__do_or_fatal__cmd" "$@" || {
-        printf "do_or_fatal: failed to %s %s\n" "$1" "$@" 1>&2
+        printf "do_or_fatal: failed to %s %s\n" "$__do_or_fatal__cmd" "$*" 1>&2
         exit 1
     }
 }
 
+set -eu
+
 do_or_fatal mkdir -p out
 
-do_or_fatal cp wheel/* out
+do_or_fatal cp wheel/*    out
 do_or_fatal cp shiv/bin/* out
 
 do_or_fatal cd out
+
 hash_filename="sha256sum.txt"
 for file in *; do
-    printf "%s %s\n" "$(do_or_fatal nix hash file --base16 "$file")" "$file"
+    hash_output="$(do_or_fatal nix hash file --base16 "$file")"
+    printf "%s %s\n" "$hash_output" "$file"
 done | {
     # shellcheck disable=SC2094
     grep -v "$hash_filename" > "$hash_filename" # this is OK, we are NOT reading and writing to a file in the same pipeline
